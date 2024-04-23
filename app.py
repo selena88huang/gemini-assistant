1e34e8e2a0dc69fb50002f3796265d95f08cb389
selena88huang <selena88.huang@gmail.com>
2024-04-24 02:03:16 +0800

    gemini assistant deploy on render

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.environ.get('CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.environ.get('CHANNEL_SECRET'))
-
+genai.configure(api_key = os.environ.get('API_KEY'))
+model = genai.GenerativeModel('gemini-pro')
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -29,8 +30,6 @@ def handle_message(event):
     line_bot_api.reply_message(event.reply_token, gemini_response)
 
 def get_gemini_response(user_message):
-    genai.configure(api_key = os.environ.get('API_KEY'))
-    model = genai.GenerativeModel('gemini-pro')
     response = model.generate_content(user_message)
     return response
 

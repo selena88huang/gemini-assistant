0dc6f71692c6f0657bc0d2383001b6554d9942ff
selena88huang <selena88.huang@gmail.com>
2024-04-24 01:26:23 +0800

    gemini assistant deploy on render

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import *
+import google.generativeai as genai
+import os
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi(os.environ.get('CHANNEL_ACCESS_TOKEN'))
+handler = WebhookHandler(os.environ.get('CHANNEL_SECRET'))
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = TextSendMessage(text=event.message.text)
+    gemini_response = get_gemini_response(user_message)
+    line_bot_api.reply_message(event.reply_token, message)
+
+def get_gemini_response(user_message):
+    genai.configure(api_key = os.environ.get('API_KEY'))
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(user_message)
+    return response
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
